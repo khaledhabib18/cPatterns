7bb6e4fe24e5b5a9329dd057893fb49cbdbc90b2
Khaled Habib <khaled.habib18@gmail.com>
2024-09-10 01:34:54 +0300

    Num Hollow Half Pyramid

---
+++ 010_Num_Hollow_Half_Pyramid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+unsigned int num=0;
+int main(void)
+{
+    printf("enter the number :  ");
+    scanf("%d",&num);
+    for (int i =1 ; i<=num ; i++)
+    {
+        for(int j = 1 ; j<=i ; j++)
+        {
+            if ((j==1) || (j==i) || (i==num))
+            {
+                printf("%i",j);
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
