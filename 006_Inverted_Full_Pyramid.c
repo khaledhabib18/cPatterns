f59721a90376b9780f1bdbe6d44f927dee589b5f
Khaled Habib <khaled.habib18@gmail.com>
2024-09-09 22:56:18 +0300

    inverted full pyramid

--- 006_Inverted_Full_Pyramid.c
+++ 006_Inverted_Full_Pyramid.c
@@ -4,7 +4,7 @@ int main()
 {
     printf("Enter the number of rows "); 
     scanf("%d",&num);
-    for (int i = 1; i <= num; i++) {
+    for (int i = num; i >= 1; i--) {
         for (int j = i; j < num; j++) { // j = 1 
             printf(" ");
         }
